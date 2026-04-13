a3672ae001538c1eb1dc5e36590af0f9f531509f
=Meridiane <=>
2026-04-13 16:58:06 -0300

    mocks com spy

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -11,12 +11,10 @@ import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,8 +38,14 @@ class AdocaoServiceTest {
     @Mock
     private EmailService emailService;
 
+    @Spy
+    List<ValidacaoSolicitacaoAdocao> validacaoSolicitacaoAdocaos = new ArrayList<>();
+
     @Mock
-    List<ValidacaoSolicitacaoAdocao> validacaoSolicitacaoAdocaos;
+    private ValidacaoSolicitacaoAdocao validacaoSolicitacaoAdocao1;
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validacaoSolicitacaoAdocao2;
 
     @Mock
     private Pet pet;
@@ -75,5 +79,24 @@ class AdocaoServiceTest {
         assertEquals(pet, adocaoCap.getPet());
         assertEquals(tutor, adocaoCap.getTutor());
         assertEquals(dto.motivo(), adocaoCap.getMotivo());
+    }
+
+    @Test
+    void deveriaChamarValidadoresAoSolicitar(){
+        //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
+        validacaoSolicitacaoAdocaos.add(validacaoSolicitacaoAdocao1);
+        validacaoSolicitacaoAdocaos.add(validacaoSolicitacaoAdocao2);
+
+        //ACT
+        service.solicitar(dto);
+
+        //ASSERT
+        BDDMockito.then(validacaoSolicitacaoAdocao1).should().validar(dto);
+        BDDMockito.then(validacaoSolicitacaoAdocao2).should().validar(dto);
     }
 }
