fd55e7b8c021511cb0029487f08b93734933cf2c
=Meridiane <=>
2026-04-15 20:54:39 -0300

    testando controllers

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService adocaoService;
+
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitarAdocaoComDadosInvalidos() throws Exception {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+        var response = mvc.perform(
+            post("/adocoes")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        Assertions.assertEquals(400, response.getStatus());
+
+    }
+}
